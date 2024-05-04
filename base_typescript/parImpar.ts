0b8854360f8b08fd941cefe8228f608ed5c0d93a
viblasco <viviblasco5d@gmail.com>
2024-05-03 22:35:14 -0300

    primer tp parImpar

---
+++ base_typescript/parImpar.ts
@@ -0,0 +1,12 @@
+import * as rls from "readline-sync";
+let numero: number = rls.questionInt("ingrese un numero: ");
+
+if (numero == 0) {
+  console.log("el numero ingresado es 0");
+} else {
+  if (numero % 2 == 0) {
+    console.log("el numero es par");
+  } else {
+    console.log("el numero es impar");
+  }
+}
