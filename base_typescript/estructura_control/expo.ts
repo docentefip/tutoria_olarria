340e271415ffde6a0a0d2cd9430de9018940ff15
viblasco <viviblasco5d@gmail.com>
2024-05-13 18:47:34 -0300

    ejercicos de consulta

--- base_typescript/estructura_control/expo.ts
+++ base_typescript/estructura_control/expo.ts
@@ -1,11 +1,25 @@
-import * as rls from 'readline-sync';
-let base:number = rls.questionInt ("ingrese numero de base: ");
-let exponente:number = rls.questionInt ("ingrese numero exponente: ");
+import * as readlineSync from 'readline-sync';
 
-let resultado:number = base**exponente;
-if (exponente==0) {
-        console.log("el resultado es 1")
-}
-        else if (resultado>0) {
-        console.log("el resultado es:  ", resultado) 
+let base : number = readlineSync.questionInt("Ingrese el número de la base: "); 
+let exponente : number = readlineSync.questionInt("Ingrese el número del exponente: "); 
+const resultado= calcularPotencia (base,exponente)
+
+function calcularPotencia (base:number,exponente:number,):number {
+    
+    if (exponente === 0) {
+        return 1;
+    } else {
+        let resultado = 1;
+        for (let i = 0; i < exponente; i++) {
+            resultado *= base;
         }
+        return resultado;
+    }
+}
+
+if (exponente < 0) {
+    console.log('El exponente debe ser mayor o igual a cero.');
+} else {
+    let resultado = calcularPotencia(base, exponente);
+    console.log('El resultado es: ' + resultado);
+}
